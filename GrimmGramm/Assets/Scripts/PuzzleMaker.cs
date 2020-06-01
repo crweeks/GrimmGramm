671cae4c52f257b4eb3f10e2cc90ef30d20dd264
Brian <bgdillard@knox.edu>
2020-05-31 19:09:32 -0500

    Mute Buttons and UI
    Re-added mute buttons to the main menu, and added the mute UI to the puzzles

--- GrimmGramm/Assets/Scripts/PuzzleMaker.cs
+++ GrimmGramm/Assets/Scripts/PuzzleMaker.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class PuzzleMaker : MonoBehaviour
 {
     public GameObject quitButton;
+    public GameObject soundOn;
+    public GameObject soundOff;
+    public GameObject musicOn;
+    public GameObject musicOff;
+    public GameObject soundOnImage;
+    public GameObject soundOffImage;
+    public GameObject musicOnImage;
+    public GameObject musicOffImage;
     public GameObject levelSelect;
     public GameObject LockControl;
     public GameObject ClickControl;
@@ -31,6 +39,14 @@ public class PuzzleMaker : MonoBehaviour
         if (puzzle_number == -1)
         {
             quitButton.SetActive(false);
+            soundOn.SetActive(false);
+            soundOnImage.SetActive(false);
+            soundOff.SetActive(false);
+            soundOffImage.SetActive(false);
+            musicOn.SetActive(false);
+            musicOnImage.SetActive(false);
+            musicOff.SetActive(false);
+            musicOffImage.SetActive(false);
             Color c = FadeCover.GetComponent<SpriteRenderer>().color;
             FadeCover.GetComponent<SpriteRenderer>().color = new Color(c.r, c.g, c.b, 0);
             return;
@@ -42,6 +58,27 @@ public class PuzzleMaker : MonoBehaviour
             CurrentPuzzle = Instantiate(puzzles[puzzle_number], new Vector3(0, 0, 0), Quaternion.identity);
             CurrentPuzzle.parent = this;
             quitButton.SetActive(true);
+            if (PlayerPrefs.GetInt("Music") == 1)
+            {
+                musicOn.SetActive(true);
+                musicOnImage.SetActive(true);
+            }
+            else
+            {
+                musicOff.SetActive(true);
+                musicOffImage.SetActive(true);
+            }
+            if (PlayerPrefs.GetInt("Mute") == 1)
+            {
+                soundOn.SetActive(true);
+                soundOnImage.SetActive(true);
+            }
+            else
+            {
+                soundOff.SetActive(true);
+                soundOffImage.SetActive(true);
+
+            }
         }
     }
 
